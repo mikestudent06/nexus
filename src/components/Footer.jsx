a7cf82b13223a9a94c813b9cbfe7adf315763003
michel mouhani <michel.mouhani@outlook.com>
2026-06-17 11:31:21 +0100

    push

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,9 @@ const socialLinks = [
 const Footer = () => {
   return (
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
+          ©Apkaid 2024. All rights reserved
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-light md:text-left">
-          ©Apkaid 2024. All rights reserved
         </p>
 
         <div className="flex justify-center gap-4  md:justify-start">
